b0294b8c765260803e0564ac0d90d40debbf665b
FedericoRodriguez <federicorodriguez@gmail.com>
2022-02-24 03:01:48 -0300

    Knowing events, props and state

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,19 @@
 <script>
-	export let name;
+  import Counter from './Counter.svelte'
+  export let initialCounter
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Counter />
+
+  <Counter {initialCounter} />
+
+  <Counter initialCounter={10} />
 </main>
+<span>Out Side Styles for this span</span>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  span {
+    color: #03f;
+  }
 </style>
---
+++ src/Counter.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let initialCounter = 0
+  let contador = initialCounter
+  function handleClick() {
+    contador++
+  }
+</script>
+
+<button on:click={handleClick}>Contador</button>
+<span>Amount of Cliks: {contador}!</span>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  span {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 1em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
